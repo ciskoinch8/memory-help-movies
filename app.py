95045cb05837e28f5719ecd1aaa5375e6d1bf4ad
ciskoinch8 <cissokhomoussa08@outlook.fr>
2022-03-13 21:27:08 +0100

    add movies in lw

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from PySide2 import QtWidgets, QtCore
 from movies import get_movies
+from movies import Movie 
+
 
 class App(QtWidgets.QWidget):
     def __init__(self):
@@ -7,6 +9,7 @@ class App(QtWidgets.QWidget):
         self.setWindowTitle("APP CINE")
         self.setup_ui()
         self.populate_movies()
+        self.setup_connections()
 
     def setup_ui(self):
         self.main_layout = QtWidgets.QVBoxLayout(self)
@@ -15,11 +18,16 @@ class App(QtWidgets.QWidget):
         self.btn_addMovies = QtWidgets.QPushButton("Ajouter")
         self.lw_movies = QtWidgets.QListWidget()
         self.btn_removeMovies = QtWidgets.QPushButton("Supprimer le(s)")
-        
+
         self.main_layout.addWidget(self.le_movieTitle)
         self.main_layout.addWidget(self.btn_addMovies)
         self.main_layout.addWidget(self.lw_movies)
         self.main_layout.addWidget(self.btn_removeMovies)
+
+    def setup_connections(self):
+        self.btn_addMovies.clicked.connect(self.add_movie)
+        self.btn_removeMovies.clicked.connect(self.remove_movie)
+        self.le_movieTitle.returnPressed.connect(self.add_movie) # Ajouter un film en appuyant sur la touche entrée
 
     def populate_movies(self):
         movies = get_movies()
@@ -27,8 +35,26 @@ class App(QtWidgets.QWidget):
         for movie in movies:
             lw_item = QtWidgets.QListWidgetItem(movie.title)
             lw_item.setData(QtCore.Qt.UserRole, movie)
-            self.lw_movies.addItem(movie)
+            self.lw_movies.addItem(lw_item)
 
+
+    def add_movie(self):
+        movie_title = self.le_movieTitle.text()
+        if not movie_title:
+            return False
+    
+        movie =  Movie(title=movie_title)
+        resultat = movie.add_to_movies()
+        if resultat:
+            lw_item = QtWidgets.QListWidgetItem(movie.title)
+            lw_item.setData(QtCore.Qt.UserRole, movie)
+            self.lw_movies.addItem(lw_item)
+
+        self.le_movieTitle.setText("")
+
+
+    def remove_movie(self):
+        print("On supprime un film")
 
 app = QtWidgets.QApplication([])
 win = App()
